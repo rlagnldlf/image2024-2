3edd70b1938253632891549e82fc1f326991b64b
108-0 <tltyd@naver.com>
2024-09-09 14:23:14 +0900

    9/9 커밋

--- 03polygon.py
+++ 03polygon.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+
 # img = np.full((500,500,3), 255, dtype=np.uint8)
 
 img_file = "./img/girl.jpg"
@@ -11,3 +12,6 @@ cv2.rectangle(img, (450,200), (200,450), (0,0,255), -1)
 cv2.imshow('polygon', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# 2024-9 원그리기
+
