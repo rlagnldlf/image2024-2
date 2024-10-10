7d11b2a8a4738bed8721b109479d2b1921f9ca1a
108-0 <tltyd@naver.com>
2024-10-10 16:01:19 +0900

    10 / 10

--- 90HaarFaceDetection.py
+++ 90HaarFaceDetection.py
@@ -4,7 +4,7 @@ from cv2.data import haarcascades
 
 face_cascade = cv2.CascadeClassifier('./recdata/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('./recdata/haarcascade_eye.xml')
-img = cv2.imread('./img/man_face.jpg')
+img = cv2.imread('./img/graduate.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray)
 for i in range(len(faces)):
